5141436d39a7952754d4e833d0521f97693ba347
eugeneaondo <eugeneaondo11@gmail.com>
2024-05-30 10:10:42 +0300

    add : pr-id route

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from typing import Union
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def merge_pr():
+    return {"This pr is merged"}
+
+
+@app.get("/pr/{pr_id}")
+def read_pr(pr_id : int):
+    return {"pr_id" : pr_id}
